1c8d0d561bf76920a26b42154c1a1c703288250c
Nask <tjdrms0459@naver.com>
2022-12-12 07:15:05 +0000

    feat: add flip to geometry

--- custom_augment.py
+++ custom_augment.py
@@ -8,15 +8,41 @@ from straug.noise import GaussianNoise
 
 # class geometry flip,rotate,perspective
 
-"""
+
 class geometry:
     def __init__(self):
-        self.degree = random.randint(1, 30)
+        # self.degree = random.randint(1, 30)
+        pass
 
+    """
     def rotate(self, image, bbox):
         h, w, c = image.shape
         cx, cy = w / 2, h / 2
-"""
+    """
+        
+    def hflip(self, image, bbox):
+        transform = A.HorizontalFlip(p=1)
+        length = len(bbox) 
+        new_box = np.zeros(length, 4, 2)
+        for i in range(length):
+            for j in range(4):
+                new_box[i][j][0] = weight - box[i][j][0]
+        return transform(image), new_box
+
+    
+    def vflip(self, image, bbox):
+        transform = A.VerticalFlip(p=1)
+        length = len(bbox) 
+        new_box = np.zeros(length, 4, 2)
+        for i in range(length):
+            for j in range(4):
+                new_box[i][j][1] = height - box[i][j][1]
+        return transform(image), new_box
+    
+    def __call__(self, image, bbox):
+        transform = random.sample([hflip, vflip], 1)
+        return transform(image, bbox)
+
 
 
 class noise:
@@ -82,26 +108,28 @@ class process:
     def __call__(self, image):
         transform = random.sample(self.transform_list, 1)
         return transform(image=image)["image"]
-
-
+    
+    
 class augment:
     def __init__(self):
-        # self.geometry = geometry()
+        self.geometry = geometry()
         self.blur = blur()
         self.noise = noise()
         self.weather = weather()
         self.camera = camera()
         self.process = process()
-
+        
     def call(self, img, bbox):
         transform_list = random.sample(
             [
-                # self.geometry,
+                self.geometry,
                 self.blur,
                 self.noise,
                 self.weather,
                 self.camera,
                 self.process,
+                self.hflip,
+                self.vflip,
             ],
             2,
         )
