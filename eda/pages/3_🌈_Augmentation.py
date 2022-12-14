a14db89f0662c2f86941147846dd093dffd71493
sshinohs <sshinohs@gmail.com>
2022-12-14 01:36:41 +0000

    add: augmentation page

--- eda/pages/3_🌈_Augmentation.py
+++ eda/pages/3_🌈_Augmentation.py
@@ -2,12 +2,30 @@ import streamlit as st
 
 import os
 
-# from typing import List
-# from tools.utils import *
-# from tools.view import *
+from typing import List
+from tools.utils import *
+from tools.view import *
 
 st.set_page_config(
     page_title="Image Augmentation Viewer",
     page_icon="🌈",
-    layout="wide",
 )
+
+with st.sidebar:
+    datasets = get_data_dirs()
+
+    dataset_path = os.path.join(
+        DATA_DIR_PATH,
+        st.selectbox(
+            "Select Dataset", datasets, on_change=change_page_session, args=[0]
+        ),
+    )
+    if "test" == dataset_path:
+        ann_path = os.path.join(DATA_DIR_PATH, dataset_path, "ufo/output.json")
+    else:
+        ann_path = os.path.join(DATA_DIR_PATH, dataset_path, "ufo/train.json")
+
+    df = set_image(ann_path)
+    set_page_session()
+    group, dataset_path, path = view_img_selector(df, dataset_path)
+view_image(group, dataset_path, path)
