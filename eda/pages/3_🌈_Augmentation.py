ac5658562414a497c3ce8ae4a57e63b4376a9afc
sshinohs <sshinohs@gmail.com>
2022-12-14 01:18:58 +0000

    add: augmentation page

---
+++ eda/pages/3_🌈_Augmentation.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+import os
+
+# from typing import List
+# from tools.utils import *
+# from tools.view import *
+
+st.set_page_config(
+    page_title="Image Augmentation Viewer",
+    page_icon="🌈",
+    layout="wide",
+)
